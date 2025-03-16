a65ab198f8d8b60884e107aef5b4ba1346d406da
kohj <080821@naver.com>
2025-03-16 22:52:29 +0900

    페이지 이동

--- DiaryProject/src/App.jsx
+++ DiaryProject/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom"; //Link 컴포넌트 = a태그
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
 import New from "./pages/New";
@@ -9,10 +9,28 @@ import Notfound from "./pages/Notfound";
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+	const nav = useNavigate();
+
+	//이벤트 핸들러 함수 : 특정 조건에 따라 이동할 페이지가 다르다면 useNavigate  커스텀 훅을 사용하자.
+	const onClickButton = () => {
+		nav("/new");
+	};
 	return (
 		<>
 			{/* 전역으로 보일게 아니면 Routes 컴포넌트 외부에 배치는 적절하지 않다. */}
-			<div>모든페이지에서 보이나?</div>
+			<div>
+				{/* 
+				Link 컴포넌트
+					SSR방식으로 페이지가 렌더링된다. 새로고침되지 않고 쾌적하고 빠르다. 
+				
+				a태그
+					a태그 사용하면 SSR방식으로 새로고침되는걸 알 수 있다.*/}
+				<Link to={"/"}>Home</Link>
+				<Link to={"/new"}>New</Link>
+				<Link to={"/diary"}>Diary</Link>
+
+				<button onClick={onClickButton}>New 페이지로 이동</button>
+			</div>
 			{/*  Routes 컴포넌트 안에는 Route 컴포넌트만 적을 수 있다. 
 			path가 들어오면 아래 적힌
 			Route컴포넌트를 위에서부터 아래로 찾는다. */}
