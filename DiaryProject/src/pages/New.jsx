8b7554c8eee0e9ab5d5f528adb5e38d5c0b140ce
DESKTOP-275CC7F\EM_NB163 <hyeonjeong.ko@easymedia.net>
2025-04-03 17:41:36 +0900

    일기장 작업중 6-1 (뒤로가기)

--- DiaryProject/src/pages/New.jsx
+++ DiaryProject/src/pages/New.jsx
@@ -5,14 +5,16 @@ import Editor from "../components/Editor"
 import {useNavigate} from "react-router-dom"
 
 const New = () => {
-    const nav = useNavigate();
     // const [params, setParams] = useSearchParams();
     // console.log(params.get("value")); //http://localhost:5173/new?value=1
+    const nav = useNavigate();
+
     return (
         <div>
             <Header
                 title={"새 일기 쓰기"}
-                leftChild={<Button text={"< 뒤로 가기"}  onClick={()=>{nav("/")}}/>}
+                // nav(-1)은 뒤로가기
+                leftChild={<Button text={"< 뒤로 가기"}  onClick={()=>{nav(-1)}}/>}
             />
             <Editor/>
         </div>
