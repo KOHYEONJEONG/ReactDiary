34f60314d8256473fe3bced0929ef39ab2dce763
kohj <080821@naver.com>
2025-04-07 23:11:22 +0900

    일기장 작업중9 (상세화면 수정중)

--- DiaryProject/src/pages/Diary.jsx
+++ DiaryProject/src/pages/Diary.jsx
@@ -1,19 +1,25 @@
-import {useParams} from "react-router-dom"
-import {useSearchParams} from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import Button from "../components/Button";
 import Header from "../components/Header.jsx";
+import Viewer from "../components/Viewer.jsx";
 
 const Diary = () => {
-    const params =useParams();
+	const params = useParams();
+	const nav = useNavigate();
 
-    return <div>
-        <Header
-            title={"yyyy-mm-dd 기록"}
-        >
+	return (
+		<div>
+			<Header
+				title={"yyyy-mm-dd 기록"}
+				leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+				rightChild={<Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />}
+			></Header>
 
-        </Header>
+			{/* 상세화면 */}
+			<Viewer></Viewer>
+		</div>
+	);
+};
 
-    </div>;
- }
-
- export default Diary;
+export default Diary;
